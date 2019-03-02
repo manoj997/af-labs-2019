b980fbda944f6caae8451ddb5160db2dcd77460f
amashi996 <amasandunikab@gmail.com>
2019-03-02 13:20:15 +0530

    add user app

--- lab-03/dist/main.65f60760.js
+++ lab-03/dist/main.65f60760.js
@@ -26448,7 +26448,105 @@ if ("development" !== 'production') {
   // http://fb.me/prop-types-in-prod
   module.exports = require('./factoryWithThrowingShims')();
 }
-},{"react-is":"node_modules/react-is/index.js","./factoryWithTypeCheckers":"node_modules/prop-types/factoryWithTypeCheckers.js"}],"User.jsx":[function(require,module,exports) {
+},{"react-is":"node_modules/react-is/index.js","./factoryWithTypeCheckers":"node_modules/prop-types/factoryWithTypeCheckers.js"}],"AddUser.jsx":[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var AddUser =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(AddUser, _Component);
+
+  _createClass(AddUser, null, [{
+    key: "propTypes",
+    get: function get() {
+      return {
+        addUser: _propTypes.default.func
+      };
+    }
+  }]);
+
+  function AddUser(props) {
+    _classCallCheck(this, AddUser);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(AddUser).call(this, props));
+  }
+
+  _createClass(AddUser, [{
+    key: "onNameChange",
+    value: function onNameChange() {
+      event.preventDefault();
+      event.stopPropagation();
+      this.name = event.target.value;
+    }
+  }, {
+    key: "onSubmit",
+    value: function onSubmit(event) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (this.name) {
+        this.props.addUser({
+          name: this.name
+        });
+        this.name = '';
+      }
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this = this;
+
+      return _react.default.createElement("div", null, _react.default.createElement("form", {
+        onSubmit: function onSubmit(event) {
+          return _this.onSubmit(event);
+        }
+      }, _react.default.createElement("label", null, "Name:"), _react.default.createElement("input", {
+        type: "text",
+        onChange: function onChange(event) {
+          return _this.onNameChange(event);
+        }
+      }), _react.default.createElement("button", {
+        type: "submit"
+      }, "Add")));
+    }
+  }]);
+
+  return AddUser;
+}(_react.Component);
+
+exports.default = AddUser;
+},{"react":"node_modules/react/index.js","prop-types":"node_modules/prop-types/index.js"}],"User.jsx":[function(require,module,exports) {
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -26551,6 +26649,8 @@ exports.default = void 0;
 
 var _react = _interopRequireWildcard(require("react"));
 
+var _AddUser = _interopRequireDefault(require("./AddUser"));
+
 var _Users = _interopRequireDefault(require("./Users"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -26596,9 +26696,27 @@ function (_Component) {
   }
 
   _createClass(AppContainer, [{
+    key: "addUser",
+    value: function addUser(user) {
+      this.setState(function (state) {
+        return {
+          users: state.users.concat({
+            id: Date.now(),
+            name: user.name
+          })
+        };
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
-      return _react.default.createElement("div", null, _react.default.createElement("h2", null, "Users App"), _react.default.createElement(_Users.default, {
+      var _this2 = this;
+
+      return _react.default.createElement("div", null, _react.default.createElement("h2", null, "Users App"), _react.default.createElement(_AddUser.default, {
+        addUser: function addUser(user) {
+          return _this2.addUser(user);
+        }
+      }), _react.default.createElement(_Users.default, {
         users: this.state.users
       }));
     }
@@ -26608,7 +26726,7 @@ function (_Component) {
 }(_react.Component);
 
 exports.default = AppContainer;
-},{"react":"node_modules/react/index.js","./Users":"Users.jsx"}],"main.jsx":[function(require,module,exports) {
+},{"react":"node_modules/react/index.js","./AddUser":"AddUser.jsx","./Users":"Users.jsx"}],"main.jsx":[function(require,module,exports) {
 'use strict';
 
 var _react = _interopRequireDefault(require("react"));
