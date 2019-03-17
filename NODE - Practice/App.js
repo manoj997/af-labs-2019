9203083f23e8c666f767e9ebf7e158e52f68038d
manoj997 <dharmakumar338@gmail.com>
2019-03-17 17:50:27 +0530

    ghghg

---
+++ NODE - Practice/App.js
@@ -0,0 +1,23 @@
+// const os = require('os');
+//
+// console.log('Architecture '+ os.arch());
+// console.log('CPUs '+os.cpus.length);
+// console.log('OS '+os.platform());
+
+//---------------------------------------------
+
+const fs=require('fs');
+// const fileName='test.txt';
+//
+// fs.readFile(fileName,(err,data)=>{
+//    if(err){
+//        console.error(err);
+//    }
+//    console.log(data);
+// });
+
+//----
+
+
+
+//--------------------------------------------
